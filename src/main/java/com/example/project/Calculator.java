57df96a5734860209b96a45a7a5d0fdaa9b3572d
Zenan0 <zenanl@nycstudents.net>
2024-09-25 15:56:30 +0000

    message

--- src/main/java/com/example/project/Calculator.java
+++ src/main/java/com/example/project/Calculator.java
@@ -1,12 +1,13 @@
 package com.example.project;
 public class Calculator{
     public String calcModel;
-    
+   
     //constructor that creates an instance of the calculator class
-    public Calculator() {
+    public Calculator(String model) {
         //implement code here
+        calcModel = model;
     }
-    
+   
     // method that performs the operation designated by operand (which
     // could be +, -, *, or /) and returns the result of that operation
     // performed between num1 and num2
@@ -16,37 +17,63 @@ public class Calculator{
     // (THIS METHOD DOES NOT PRINT)
     public double performOperation(String operand, int num1, int num2) {
         //implement code here
-        return 0.00;
+        if (operand .equals("+")) {
+            return num1 + num2;
+        } if (operand .equals("-")) {
+            return num1 - num2;
+        } if (operand .equals("/")) {
+            return num1 / num2;
+        } if (operand .equals("*")) {
+            return num1 * num2;
+        } else {
+            return 0.00;
+        }
     }
+
 
     // method that checks if num1 is evenly divisible by num2, and if so,
     // returns true, and if not, returns false
     // (THIS METHOD DOES NOT PRINT)
     public boolean divisibleBy(int num1, int num2) {
         //implement code here
-        return false;
+        int place = num1 % num2;
+        if (place == 0) {
+            return true;
+        } else {
+            return false;
+        }
+
+
     }
+
 
     // method that constructs and returns a string representing a coordinate
     // pair in the format: "(x, y)"
     // (THIS METHOD DOES NOT PRINT)
     public String coordinatePair(int x, int y) {
         //implement code here
-        return "";
+        return "(" + x + "," + y + ")";
     }
     // method that determines and returns the result of |num1 - num2|
     // (THIS METHOD DOES NOT PRINT)
     public int absoluteValue(int num1, int num2) {
         //implement code here
-        return 0;
+        int result = num1 - num2;
+        if (result < 0) {
+            return result * -1;
+        } else if (result == 0) {
+            return 0;
+        } else {
+            return result;
+        }
     }
-    
+   
     // method that constructs and returns a String in the format:
     // "This calculator is a model "+____+" where "+____+" should be filled in
     // with the value stored in the calcModel instance variable."
     // (THIS METHOD DOES NOT PRINT)
     public String info() {
         //implement code here
-        return "";
+        return "This calculator is a model "+ calcModel + " where " + calcModel + " should be filled in with the value stored in the calcModel instance variable.";
     }
 }
--- src/main/java/com/example/project/CalculatorRunner.java
+++ src/main/java/com/example/project/CalculatorRunner.java
@@ -1,12 +1,38 @@
 package com.example.project;
 
+
 public class CalculatorRunner {
     public static void main(String[] args) {
 
+
         //test your program here
         //1. create an instance of the calculator class
+        Calculator calc1 = new Calculator("TI84");
+        System.out.print(calc1.info());
+
 
         //2. call any methods of that class to test
 
+
+        System.out.println(calc1.performOperation("+", 5, 6));
+        System.out.println(calc1.performOperation("-", 5, 6));
+        System.out.println(calc1.performOperation("/", 5, 6));
+        System.out.println(calc1.performOperation("*", 5, 6));
+        System.out.println(calc1.performOperation("G", 5, 6));
+
+
+        System.out.println(calc1.divisibleBy(10, 5));
+        System.out.println(calc1.divisibleBy(7, 4));
+
+
+        System.out.println(calc1.coordinatePair(7, 8));
+
+
+        System.out.println(calc1.absoluteValue(55, 100));
+
+
+
+
     }
 }
+
